9cb884d5c24a0ed9d73f067e64a9cd22e5ac1392
GheorgheZZ <gheorghe.zavidei@gmail.com>
2023-04-28 23:20:55 +0300

    recap1.1

--- Recapitulare/TPA_1.cpp
+++ Recapitulare/TPA_1.cpp
@@ -10,8 +10,21 @@ int main () {
 
     std::cout << "Ce varsta aveti?" << std::endl;
     std::cin >> varsta;
-    std::cout << "Super, ai " <<varsta <<" de ani";
+    std::cout << "Super, ai " <<varsta <<" de ani"<<std::endl;
 
+/*2.Creati un program ce sa va intrebe in ce an v-ati nascut si sa printeze pe ecran ce varsta aveti.
+
+Ex:
+In ce an v-ati nascut?
+1991
+Super, aveti 31 de ani*/
+    int an_curent=2023;
+    int an_nastre;
+    int ani;
+
+    std::cout <<"In ce an v-ati nascut?" << std::endl;
+    std::cin >> an_nastre;
+    std::cout << "Super, aveti " << an_curent-an_nastre<<" de ani"<<std::endl;
 
 
     return 0;
